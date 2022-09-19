f0f4d2a1bfb088d2febc3da782177854397c5e6f
deondreE <deondreenglish45@gmail.com>
2022-09-19 08:50:32 -0500

    Working roadmap page

--- packages/web/pages/roadmap/index.tsx
+++ packages/web/pages/roadmap/index.tsx
@@ -1,35 +1,41 @@
 
 import Image from 'next/image';
+import ProgressBar from '../../components/Roadmap/ProgressBar';
 import folder from '../../public/folder.svg'; 
-import styled from 'styled-components';
 
 import '../../styles/Home.module.css';
 
-const Conatiner = styled.div`
-    width: 50%;
-    height: 50%;
-    display: flex;  
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    background-color: #f5f5f5;
-    border-radius: 10px;
-    positoon: relative;
-    right: 100em;
-`;
-
 function Index () {
+    const testData = [
+        { bgcolor: "#6a1b9a", completed: 10, name: "Design" },
+        { bgcolor: "#00695c", completed: 20, name: "Development" },
+        { bgcolor: "#ef6c00", completed: 10, name: "Testing" },
+    ];
+
     return (
         <div >
             {/* Center using tailwind */}
             <div className=''>
-                <Conatiner>
+                <div className='text-center mt-12'>
                     <Image src={folder} alt='folder' width={100} height={100} />
-                </Conatiner>
-                <div className='text-1xl text-center'>
-                    <h1 className='text-3xl text-center'>My Roadmap</h1>
+                    <h1 className='text-4xl text-center mb-2'>My Roadmap</h1>
                     <p className='font-light'>Here is a list of features we are working on, and the progress we have made so far.</p>
                 </div>
+            </div>
+            {testData.map((item, idx) => {
+                return (
+                    <div key={idx} className='mt-10 block justify-center border w-96 p-4 relative right-0'>
+                        <p className='text-3xl text-zinc-700 font-bold text-center'>{item.name}</p>
+                        <div className='w-96 justify-center text-center'>
+                            <ProgressBar bgcolor={item.bgcolor} completed={item.completed} />
+                        </div>
+                    </div>
+                )
+            })}
+
+            <div>
+                <p className='text-center text-2xl text-zinc-700 font-bold mt-10'>Want to see more?</p>
+                <i className='text-center text-1xl'>In Development</i>
             </div>
         </div>
     )
