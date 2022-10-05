ef577bdad42fbe0d2d3a98ad59631f1813b46c23
deondreE <deondreenglish45@gmail.com>
2022-10-05 10:10:01 -0500

    macos-app created project in xcode NOT WORKING IN HERE YET

---
+++ packages/macos-app/macos-app/macos-app/macos_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  macos_appApp.swift
+//  macos-app
+//
+//  Created by Deondre English on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct macos_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
